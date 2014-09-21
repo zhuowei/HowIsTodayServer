a3e2c61c9d1996ee72af0b794c49b1308857b705
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2014-09-20 22:24:15 -0400

    Don't crash if no subject is passed in

--- src/main/java/com/kylemsguy/howistoday/MyResource.java
+++ src/main/java/com/kylemsguy/howistoday/MyResource.java
@@ -71,6 +71,7 @@ public class MyResource {
 	}
 
 	private String[] getSecuritiesByCategory(String name) {
+		if (name == null) return null;
 		if (name.equals("Finance")) {
 			return new String[] {
 				"JPM US Equity",

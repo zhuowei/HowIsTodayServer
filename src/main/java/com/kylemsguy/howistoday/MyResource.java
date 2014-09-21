8218f05ac141f39d121edccb72bf40bcf0e0306a
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2014-09-20 20:57:02 -0400

    List of stock names to check

--- src/main/java/com/kylemsguy/howistoday/MyResource.java
+++ src/main/java/com/kylemsguy/howistoday/MyResource.java
@@ -19,13 +19,21 @@ public class MyResource {
 	 * @return String that will be returned as a text/plain response.
 	 */
 	@GET
-	public String getIt(@QueryParam("a") String wat) {
+	public String getIt(@QueryParam("subject") String subject) {
 		try {
+			String[] stockNames = getSecuritiesByCategory(subject);
+			if (stockNames == null) {
+				// DISABLE THIS IN PROD!!!1111
+				stockNames = new String[] {subject};
+			}
 			Session session = BloombergApiManager.getSession();
+			double sum = 0;
 			synchronized(session) {
 				Service refService = BloombergApiManager.getService("//blp/refdata");
 				Request req = refService.createRequest("ReferenceDataRequest");
-				req.append("securities", wat);
+				for (String s: stockNames) {
+					req.append("securities", s);
+				}
 				req.append("fields", "News Sentiment");
 				CorrelationID d_cid = session.sendRequest(req, null);
 				while (true) {
@@ -35,6 +43,10 @@ public class MyResource {
 						Message msg = msgIter.next();
 						if (msg.correlationID() == d_cid) {
 							System.out.println(msg);
+							//Element allVals = msg.getElementByName("securityData[]");
+							//for (int i = 0; i < allVals.numValues(); i++) {
+							//	//double val = allVals.get
+							//}	
 						}
 					}
 					if (event.eventType() == Event.EventType.RESPONSE) {
@@ -42,7 +54,9 @@ public class MyResource {
 					}
 				}
 			}
-			return "yo";
+			double avg = 0;
+			String headline = "Have a nice day";
+			return "{0:" + avg + ", 1:\"" + headline + "\"}";
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new RuntimeException(e);
@@ -50,11 +64,22 @@ public class MyResource {
 	}
 
 	private String[] getSecuritiesByCategory(String name) {
-		if (name.equals("finance")) {
+		if (name.equals("Finance")) {
 			return new String[] {
-				"CM CN Equity"
+				"JPM US Equity",
+				"BAC US Equity",
+				"C US Equity",
+				"WFC US Equity"
+			};
+		} else if (name.equals("Science and Technology")) {
+			return new String[] {
+				"AAPL US Equity",
+				"GOOG US Equity",
+				"AMZN US Equity",
+				"MSFT US Equity"
 			};
 		}
+
 		return null;
 	}
 }
